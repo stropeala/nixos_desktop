8d3433a4b9dd2bdaeae511626ba1508405a8ba46
Petre Razvan <petre.ispir2002@protonmail.com>
2026-08-23 19:02:13 +0300

    added back harware config plus home manager and fetch

--- flake.nix
+++ flake.nix
@@ -4,15 +4,55 @@
   #========  INPUTS
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    #========  APPS
+    # areofyl-fetch
+    areofyl-fetch = {
+      url = "github:areofyl/fetch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    #========  HOME MANAGER
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   #========  OUTPUTS
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      areofyl-fetch,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+          #========  APPS
+          # areofyl-fetch
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [
+              areofyl-fetch.overlays.default
+            ];
+            environment.systemPackages = [
+              areofyl-fetch.packages.${pkgs.system}.default
+            ];
+          })
+
+          #========  HOME MANAGER MODULES
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixostrop = import ./home_manager.nix;
+          }
+        ];
+      };
     };
-  };
 }
